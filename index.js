1a768ace93261a0b600d8df760da9f3b6cab678d
Alabhya Jindal <alabhya10@gmail.com>
2023-02-14 22:03:05 +0530

    Added home page Extracted logic to two functions

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const showdown = require('showdown');
 const fs = require('fs');
 
-fs.readdir('./source', (err, files) => {
+function createHomePage() {
+  const src = fs.readFileSync('./source/index.html', 'utf-8');
+  fs.mkdir('./dist', { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    fs.closeSync(fs.openSync('./dist/index.html', 'w'));
+    fs.writeFile('./dist/index.html', src, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+}
+
+function createBlogPages({ files }) {
   files.forEach((file) => {
     const converter = new showdown.Converter();
-    const text = fs.readFileSync(`./source/${file}/index.md`, 'utf-8');
+    const text = fs.readFileSync(`./source/blog/${file}/index.md`, 'utf-8');
     const html = converter.makeHtml(text);
     const css = fs.readFileSync('./style.css', 'utf-8');
     const src = `
@@ -26,4 +41,12 @@ fs.readdir('./source', (err, files) => {
       });
     });
   });
+}
+
+fs.readdir('./source/blog', (err, files) => {
+  if (err) {
+    console.error(err);
+  }
+  createHomePage();
+  createBlogPages({ files });
 });
