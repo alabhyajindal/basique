4ade3d4c2045b8ee85d0319f699bf5ed2517d079
Alabhya Jindal <alabhya10@gmail.com>
2023-02-17 00:14:30 +0530

    Copying the entire directory

--- zeta-ssg/createSource.js
+++ zeta-ssg/createSource.js
@@ -1,29 +1,46 @@
 const fs = require('fs');
+const path = require('path');
 
-const PROJECT_NAME = 'my-zeta-app';
+function createProjectFolder(projectPath) {
+  fs.mkdirSync(projectPath);
+}
 
-function createHomePage() {
-  const src = fs.readFileSync(`${__dirname}/sourceFiles/index.html`, 'utf-8');
-  const sourcePath = `./${PROJECT_NAME}/source`;
+function copyDirectory(source, destination) {
+  // Create the destination directory if it doesn't exist
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination);
+  }
 
-  fs.mkdir(`./${PROJECT_NAME}`, { recursive: true }, (err) => {
-    fs.mkdir(sourcePath, { recursive: true }, (err) => {
-      if (err) {
-        console.error(err);
-      }
-      fs.closeSync(fs.openSync(`${sourcePath}/index.html`, 'w'));
-      fs.writeFile(`${sourcePath}/index.html`, src, (err) => {
-        if (err) {
-          console.error(err);
-        }
-      });
-    });
-  });
+  // Get the list of files and folders in the source directory
+  const files = fs.readdirSync(source);
+
+  // Loop through the files and folders
+  for (const file of files) {
+    // Get the full path of the file or folder
+    const sourcePath = path.join(source, file);
+    const destinationPath = path.join(destination, file);
+
+    // Get the stats for the current file or folder
+    const stats = fs.statSync(sourcePath);
+
+    // If it's a file, copy it to the destination
+    if (stats.isFile()) {
+      fs.copyFileSync(sourcePath, destinationPath);
+    }
+    // If it's a directory, recursively call the copyDirectory function
+    else if (stats.isDirectory()) {
+      copyDirectory(sourcePath, destinationPath);
+    }
+  }
 }
 
 function main() {
+  const PROJECT_NAME = 'my-zeta-app';
+  const projectPath = `${process.cwd()}/${PROJECT_NAME}`;
+
   console.log('Creating your project...');
-  createHomePage();
+  createProjectFolder(projectPath);
+  copyDirectory(`${__dirname}/sourceFiles`, projectPath);
   console.log('Project created!');
 }
 
